7b9575cb4c748bf1a281e0df1b1bf1ba3cc4dca8
coirne <82501778+coirne@users.noreply.github.com>
2021-05-25 17:46:17 +0200

    update commentaires

--- src/main/java/tn/devops/demo/DemoApplication.java
+++ src/main/java/tn/devops/demo/DemoApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 // we initialize the Servlet context required by Tomcat by implementing the SpringBootServletInitializer interface
 public class DemoApplication extends SpringBootServletInitializer  {
-
+ /**
+  * La méthode main ...
+  * @param args les arguments 
+  * de la méthode main
+  */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
